c92ef85ee495681145fc2deba15fb6ccf0256fb3
stevekutz <stkutz@gmail.com>
2020-09-29 18:56:07 -0500

    created new SECRET_KEY and set up .env file

--- crud_proj/settings.py
+++ crud_proj/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '71pktoug%+pd%vq0p(_vbe9_lp@r_&!-j9no85w+^p@51nbgcj'
+# SECRET_KEY = '71pktoug%+pd%vq0p(_vbe9_lp@r_&!-j9no85w+^p@51nbgcj'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+# DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
